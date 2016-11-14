db75e6816c095ba9868ad7eb467556e583420053
Wen Chen <wc2442@columbia.edu>
2016-11-14 16:01:20 -0500

    use setUp

--- src/test/java/org/wc2442/interview/questions/TestMyDoubleLinkedList.java
+++ src/test/java/org/wc2442/interview/questions/TestMyDoubleLinkedList.java
@@ -7,15 +7,12 @@ import org.junit.Test;
 
 public class TestMyDoubleLinkedList {
 
+	private MyDoubleLinkedList one;
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@Test
-	public void test() {
 		MyDoubleLinkedList three = new MyDoubleLinkedList(3);
 		MyDoubleLinkedList two = new MyDoubleLinkedList(2);
-		MyDoubleLinkedList one = new MyDoubleLinkedList(1);
+		one = new MyDoubleLinkedList(1);
 		one.setNext(two);
 		two.setPrev(one);
 		two.setNext(three);
@@ -25,11 +22,14 @@ public class TestMyDoubleLinkedList {
 		MyDoubleLinkedList next = one.getNext();
 		assertEquals(new Integer(2), next.getElement());
 		next = next.getNext();
-		assertEquals(new Integer(3), next.getElement());
-		
+		assertEquals(new Integer(3), next.getElement());		
+	}
+
+	@Test
+	public void test() {
 		MyDoubleLinkedList reverse = MyDoubleLinkedList.reverse(one);
 		assertEquals(new Integer(3), reverse.getElement());
-		next = reverse.getNext();
+		MyDoubleLinkedList next = reverse.getNext();
 		assertEquals(new Integer(2), next.getElement());
 		next = next.getNext();
 		assertEquals(new Integer(1), next.getElement());		
--- src/test/java/org/wc2442/interview/questions/TestMyLinkedList.java
+++ src/test/java/org/wc2442/interview/questions/TestMyLinkedList.java
@@ -2,25 +2,31 @@ package org.wc2442.interview.questions;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestMyLinkedList {
 
-	@Test
-	public void testReverse() {
+	private MyLinkedList one;
+	
+	@Before
+	public void setUp() throws Exception {
 		MyLinkedList three = new MyLinkedList(3, null);
 		MyLinkedList two = new MyLinkedList(2, three);
-		MyLinkedList one = new MyLinkedList(1, two);
+		one = new MyLinkedList(1, two);	
 		
 		assertEquals(new Integer(1), one.getElement());
 		MyLinkedList next = one.getNext();
 		assertEquals(new Integer(2), next.getElement());
 		next = next.getNext();
-		assertEquals(new Integer(3), next.getElement());
-		
+		assertEquals(new Integer(3), next.getElement());		
+	}
+
+	@Test
+	public void testReverse() {
 		MyLinkedList reverse = MyLinkedList.reverse(one);
 		assertEquals(new Integer(3), reverse.getElement());
-		next = reverse.getNext();
+		MyLinkedList next = reverse.getNext();
 		assertEquals(new Integer(2), next.getElement());
 		next = next.getNext();
 		assertEquals(new Integer(1), next.getElement());		
@@ -28,19 +34,9 @@ public class TestMyLinkedList {
 	
 	@Test
 	public void testReverse2() {
-		MyLinkedList three = new MyLinkedList(3, null);
-		MyLinkedList two = new MyLinkedList(2, three);
-		MyLinkedList one = new MyLinkedList(1, two);
-		
-		assertEquals(new Integer(1), one.getElement());
-		MyLinkedList next = one.getNext();
-		assertEquals(new Integer(2), next.getElement());
-		next = next.getNext();
-		assertEquals(new Integer(3), next.getElement());
-
 		MyLinkedList reverse2 = MyLinkedList.reverse2(one);
 		assertEquals(new Integer(3), reverse2.getElement());
-		next = reverse2.getNext();
+		MyLinkedList next = reverse2.getNext();
 		assertEquals(new Integer(2), next.getElement());
 		next = next.getNext();
 		assertEquals(new Integer(1), next.getElement());		
